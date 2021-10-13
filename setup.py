e0e04d6b242d6f7bbdbc1d79ebf233dd9af4b4e2
duartecoelho <duarte@mov.ai>
2021-10-13 13:38:44 +0100

    [SKIP] Automatic version bump 1.0.0-1 -> 1.0.0-2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="movros-build-system",
-    version='1.0.8-10-1-1',
+    version='1.0.0-2',
     author="DevOps team",
     author_email="devops@mov.ai",
     description="Mov(ai) Ros, The framework to build, raise and package ros and ros movai packages",
